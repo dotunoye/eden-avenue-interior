420dff47a7aa78b114a9c38dc2ed021191ea05e0
Dotun <oyelamioo.24@student.funaab.edu.ng>
2026-03-20 23:56:57 +0100

    Fixed the aspect ratio for the interior page

--- eden-avenue-interiors/sanity.cli.js
+++ eden-avenue-interiors/sanity.cli.js
@@ -10,6 +10,9 @@ export default defineCliConfig({
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
+    deployment: {
+    appId: 'c2dnng6z75hsuwdoqea0ekwk',
+    },
     autoUpdates: true,
   }
 })
